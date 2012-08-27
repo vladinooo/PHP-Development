f75646290e2867dc0021d8825dee0467a3a6f22d
Vlad Hartmann <artmannv@yahoo.co.uk>
2012-08-27 17:31:35 +0100

    config edit

--- Hartmann-cms/includes/config.php
+++ Hartmann-cms/includes/config.php
@@ -2,7 +2,7 @@
 	
 // db config
 	define("DB_SERVER", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "sony_700");
-	define("DB_NAME", "pg_cms");
+	define("DB_USER", "user");
+	define("DB_PASS", "password");
+	define("DB_NAME", "database");
 ?>
